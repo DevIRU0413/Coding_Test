239e5b9d02400155ff326ab2ba6e540bdf99e5c2
DevIRU0413 <deviru0413@gmail.com>
2026-07-14 20:04:03 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.19 ms, Memory: 31.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = new int[] {0, 0};
+        foreach(int i in num_list)
+        {
+            if(i%2==0)
+                answer[0]++;
+            else    
+                answer[1]++;
+        }
+        
+        return answer;
+    }
+}
