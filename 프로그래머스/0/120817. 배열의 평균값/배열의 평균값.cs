237e544472e6c9ce590ce9610b0e03e9ca86dba9
DevIRU0413 <deviru0413@gmail.com>
2026-07-14 18:39:11 +0900

    [level 0] Title: 배열의 평균값, Time: 0.18 ms, Memory: 31.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Solution {
+    public double solution(int[] numbers) {
+        double answer = 0;
+        double sum = 0;
+        int length = numbers.Length;
+        for(int i = 0; i < length; i++)
+            sum += numbers[i];
+        answer = sum / length;
+        return answer;
+    }
+}
