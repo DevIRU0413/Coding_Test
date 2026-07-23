1543554aa4b6a44afdbd79a4d53847160b153e2e
DevIRU0413 <deviru0413@gmail.com>
2026-07-24 00:27:43 +0900

    [level 0] Title: 배열의 유사도, Time: 0.32 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.cs
@@ -0,0 +1,22 @@
+using System;
+
+public class Solution {
+    public int solution(string[] s1, string[] s2) {
+        int answer = 0;
+        
+         if (s1.Length > s2.Length) {
+            var tmp = s1; s1 = s2; s2 = tmp;
+        }
+
+        for (int i = 0; i < s1.Length; i++) {
+            for (int j = 0; j < s2.Length; j++) {
+                if (s1[i] == s2[j]) {
+                    answer++;
+                    break; 
+                }
+            }
+        }
+
+        return answer;
+    }
+}
