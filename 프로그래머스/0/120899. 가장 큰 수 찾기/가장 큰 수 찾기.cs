6f21d64d3d6570b2a343252c5e0533dce6fb39e7
DevIRU0413 <deviru0413@gmail.com>
2026-07-24 17:07:25 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.10 ms, Memory: 46.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Solution {
+    public int[] solution(int[] array) {
+        int[] answer = new int[] {0, 0};
+        for(int i = 0; i < array.Length; i++){
+            if(answer[0] < array[i]){
+                answer[0] = array[i];
+                answer[1] = i;
+            }
+        }
+        return answer;
+    }
+}
