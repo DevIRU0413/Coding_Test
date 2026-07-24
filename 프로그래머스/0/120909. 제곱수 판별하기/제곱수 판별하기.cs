cab14b14e3ab605ab86a386f835c2179faeca3e9
DevIRU0413 <deviru0413@gmail.com>
2026-07-24 16:56:17 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.17 ms, Memory: 46.4 MB -BaekjoonHub

--- 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.cs
+++ 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.cs
@@ -2,15 +2,11 @@ using System;
 
 public class Solution {
     public int solution(int n) {
-        int answer = 2;
         for(int i = 1; i * i <= n; i++)
         {
             if(i * i == n)
-            {
-                answer = 1;
-                break;
-            }
+                return 1;
         }
-        return answer;
+        return 2;
     }
 }
