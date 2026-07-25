5626f2006f3a0317e40495a0528bcf74b5085cc3
DevIRU0413 <deviru0413@gmail.com>
2026-07-25 12:09:34 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.97 ms, Memory: 46.6 MB -BaekjoonHub

--- 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.cs
+++ 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.cs
@@ -2,37 +2,7 @@ using System;
 
 public class Solution {
     public int solution(int[] array) {
-        QuickSort(array, 0, array.Length-1);
-        int answer = array[array.Length / 2];
-        return answer;
-    }
-    
-    private static void QuickSort(int[] arr, int left, int right) {
-        if (left >= right) return;
-
-        int pivotIndex = Partition(arr, left, right);
-        QuickSort(arr, left, pivotIndex - 1);
-        QuickSort(arr, pivotIndex + 1, right);
-    }
-
-    private static int Partition(int[] arr, int left, int right) {
-        int pivot = arr[right];
-        int i = left;
-
-        for (int j = left; j < right; j++) {
-            if (arr[j] < pivot) {
-                Swap(arr, i, j);
-                i++;
-            }
-        }
-
-        Swap(arr, i, right);
-        return i;
-    }
-
-    private static void Swap(int[] arr, int a, int b) {
-        int temp = arr[a];
-        arr[a] = arr[b];
-        arr[b] = temp;
+        Array.Sort(array);
+        return array[array.Length/2];
     }
 }
