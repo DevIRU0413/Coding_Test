881b6220b838eb8cd706b7c16498eaa69facef92
DevIRU0413 <deviru0413@gmail.com>
2026-07-25 13:51:00 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.87 ms, Memory: 46.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+
+public class Solution {
+    public int[] solution(string my_string) {
+        List<int> answer = new List<int>();
+        foreach(char c in my_string){
+            if(char.IsDigit(c))
+               answer.Add(c - '0');
+        }
+        answer.Sort();
+        return answer.ToArray();
+    }
+}
