3db43dd99dc8f26e104ce76f378119a515bdc1e1
DevIRU0413 <deviru0413@gmail.com>
2026-07-26 22:21:35 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 1.20 ms, Memory: 45.7 MB -BaekjoonHub

--- 프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.cs
+++ 프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.cs
@@ -1,21 +1,13 @@
 using System;
-using System.Text;
 
 public class Solution {
     public string solution(string my_string) {
-        int[] count = new int[26];
-        foreach (char c in my_string)
-        {
-            char lower = c <= 'Z' ? (char)(c + 32) : c;
-            count[lower - 'a']++;
+        char[] chars = my_string.ToCharArray();
+        for(int i = 0; i < chars.Length; i++){
+            if(chars[i] >= 'A' && chars[i] <= 'Z')
+                chars[i] = (char)(chars[i] + 32);
         }
-
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < 26; i++)
-        {
-            while (count[i]-- > 0)
-                sb.Append((char)('a' + i));
-        }
-        return sb.ToString();
+        Array.Sort(chars);
+        return new string(chars);
     }
 }
