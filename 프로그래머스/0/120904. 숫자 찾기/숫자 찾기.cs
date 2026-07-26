8b8ea8b0edc245f2e27456999533284381d8b8bd
DevIRU0413 <deviru0413@gmail.com>
2026-07-26 21:51:37 +0900

    [level 0] Title: 숫자 찾기, Time: 3.71 ms, Memory: 47.5 MB -BaekjoonHub

--- 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.cs
+++ 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.cs
@@ -2,17 +2,7 @@ using System;
 
 public class Solution {
     public int solution(int num, int k) {
-        int answer = -1;
-        int lt = (int)Math.Log10(num);
-        for(int i = lt; i >= 0; i--)
-        {
-            int pow = (int)Math.Pow(10, i);
-            int digit = num / pow % 10;
-            if(digit == k){
-                answer = lt + 1 - i;
-                break;
-            }
-        }
-        return answer;
+        int answer = num.ToString().IndexOf(k.ToString()) + 1;
+        return answer == 0 ? answer - 1 : answer;
     }
 }
