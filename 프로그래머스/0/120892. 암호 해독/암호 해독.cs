b1de5d28ac84173049b8d197a0e8135c453161ce
DevIRU0413 <deviru0413@gmail.com>
2026-07-26 21:22:02 +0900

    [level 0] Title: 암호 해독, Time: 0.60 ms, Memory: 47.3 MB -BaekjoonHub

--- 프로그래머스/0/120892. 암호 해독/암호 해독.cs
+++ 프로그래머스/0/120892. 암호 해독/암호 해독.cs
@@ -3,11 +3,9 @@ using System;
 public class Solution {
     public string solution(string cipher, int code) {
         string answer = "";
-        char[] arr = new char[cipher.Length/code];
-        for(int i = code - 1; i < cipher.Length; i += code)
-            arr[i / code] = cipher[i];
-        
-        answer = new string(arr); 
+        for(int i=0;i<cipher.Length+1;i++)
+            if(i % code == 0 && i !=0)
+                answer += cipher[i-1];
         return answer;
     }
 }
