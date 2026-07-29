3e3d369b2009f0e6165b8dd62f2aca007b4f3d20
DevIRU0413 <deviru0413@gmail.com>
2026-07-29 18:38:38 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.16 ms, Memory: 46.3 MB -BaekjoonHub

--- 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.cs
+++ 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.cs
@@ -5,11 +5,7 @@ public class Solution {
         int[] answer = new int[numbers.Length];
         int dir = (direction == "left") ? -1 : 1;
         for(int i = 0; i < numbers.Length; i++) {
-            int idx = i + dir;
-            if(idx < 0)
-                idx = numbers.Length - 1;
-            else if (idx == numbers.Length)
-                idx = 0;
+            int idx = (i + dir + numbers.Length) % numbers.Length;
             answer[idx] = numbers[i];
         }
         return answer;
