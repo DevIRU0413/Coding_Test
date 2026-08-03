3a28c767bab408a1b608523735b4b65bc979ba75
DevIRU0413 <deviru0413@gmail.com>
2026-08-03 09:59:05 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.15 ms, Memory: 45.9 MB -BaekjoonHub

--- 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.cs
+++ 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.cs
@@ -3,11 +3,10 @@ using System;
 public class Solution {
     public int[] solution(int[] emergency) {
         int[] answer =  new int[emergency.Length];
-        int count = 0;
-        
+
         for(int i = 0; i < emergency.Length; i++){
             int num = emergency[i];
-            count = 0;
+            int count = 0;
             for(int j = 0; j < emergency.Length; j++){
                 if(num < emergency[j])
                     count++;
