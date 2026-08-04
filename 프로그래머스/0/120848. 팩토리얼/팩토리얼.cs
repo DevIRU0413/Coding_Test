b8b092237dd6acacd237c58ba0cb33e58d4a82cc
DevIRU0413 <deviru0413@gmail.com>
2026-08-04 10:16:40 +0900

    [level 0] Title: 팩토리얼, Time: 0.14 ms, Memory: 45.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120848. 팩토리얼/팩토리얼.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class Solution {
+    public int solution(int n) {
+        int fact = 1;
+        int i = 1;
+
+        while (fact <= n)
+        {
+            i++;
+            fact *= i;
+        }
+
+        return i - 1;
+    }
+}
