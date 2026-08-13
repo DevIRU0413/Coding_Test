ac14cc4ffa5ea3d95f62036f476d39c17d3da518
DevIRU0413 <deviru0413@gmail.com>
2026-08-13 15:20:07 +0900

    [level 0] Title: 컨트롤 제트, Time: 1.09 ms, Memory: 47.7 MB -BaekjoonHub

--- 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.cs
+++ 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.cs
@@ -3,34 +3,16 @@ using System;
 public class Solution {
     public int solution(string s) {
         int answer = 0;
-        int addNum = 0;
         int num = 0;
-        bool minus = false;
-        foreach(char c in s){
-            if(' ' == c){
-                if(minus)
-                    num *= -1;
-                answer += num;
-                addNum = num;
-                num = 0;
-                minus = false;
+        foreach(string c in s.Split(' '))
+        {
+            if(c == "Z"){
+                answer -= num;
                 continue;
             }
-            else if('Z' == c){
-                answer -= addNum;
-                continue;
-            }
-            else if('-' == c){
-                minus = true;
-                continue;
-            }
-            
-            num *= 10;
-            num += c - '0';
+            num = int.Parse(c);
+            answer += num;
         }
-        if (minus)
-            num *= -1;
-        answer += num;
         return answer;
     }
 }
